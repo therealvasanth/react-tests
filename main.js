c8c1d005fa98ceecd7fa3492686a8a061eec5387
therealvasanth <therealvasanth@gmail.com>
2018-07-02 23:30:49 +0530

    learning component composing

--- main.js
+++ main.js
@@ -4,6 +4,22 @@ function Navbar(props){
 	return <p>welcome back, {props.name}</p>;
 }
 
-const ele = <Navbar name="Niharika" />;
+//const ele = <Navbar name="Niharika" />;
 
-ReactDOM.render(ele, document.getElementById("root"));
+// composing components
+
+/** so this is how an app is made as one which is really composed of multiple components **/
+
+function Ele(){
+	return(
+		<div>
+			<Navbar name="john doe" />
+			<Navbar name="jane doe" />
+			<Navbar name="lil john doe" />
+			<Navbar name="lil jane doe" />
+		</div>
+	);
+}
+
+ReactDOM.render(<Ele />, document.getElementById("root"));
+
